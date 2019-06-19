409703f7292ff584d2f0749fab07613b829064a8
Valentino <vcappelletti@udesa.edu.ar>
2019-06-19 19:06:16 -0300

    Hice el buscador para buscar peliculas y ver los reviews

--- peliculas.js
+++ peliculas.js
@@ -1,3 +1,56 @@
 window.onload = function() {
+API_KEY = "928ad4dee3a02646fa1725b8bcaa2a96"
+var url = " https://api.themoviedb.org/3/movie/popular?api_key="+API_KEY+"&page=1"
+fetch(url)
+.then(function(response){
+  return response.json();
+})
+.then(function(objetoLiteralRespuesta) {
+console.log(objetoLiteralRespuesta);
+var arrayPeliculas = objetoLiteralRespuesta.results
 
+var ul = 
+
+for (var i = 0; i < arrayPeliculas.length; i++) {
+  arrayPeliculas[i]
+  // li = "<li>"
+  // li +=    "<div class='uk-panel'>"
+  // li +=        "<img src=''>"
+  // li +=         "<p>"+   title +"</p>"
+  // li +=    "</div>"
+  // li +="</li>"
+}
+
+})
+.catch(function(error) {
+  console.log("the error was: " + error);
+})
+
+
+
+}
+// CUANDO INGRESO; DEBO INICIALIZAR EL ARRAY DONDE VOY A GUARDAR LAS PELIS FAVORITAS
+var arrayDePelisFavoritas = [] // ESTO IRIA LUEGO DE HACER LOG IN
+
+function agregarFavoritos(id) {
+ alert("me clickearon!!!")
+ // PRIMERO, reviso si hay alguna peli FAVORITA (en el array)
+ if (arrayDePelisFavoritas.indexOf(id)===-1) {
+     // EN ESTE CASO NO ES FAVORITA
+     // pusheo el id dentro del array
+     arrayDePelisFavoritas.push(id)
+     // guardo en session el array, como es un objeto debo transformarlo a STRING
+     window.sessionStorage.setItem("favorita",JSON.stringify(arrayDePelisFavoritas))
+ } else {
+   // ESTA PELI YA ES FAVORITA
+   console.log(arrayDePelisFavoritas.indexOf(id));
+   // la saco del array
+   arrayDePelisFavoritas.splice(arrayDePelisFavoritas.indexOf(id),1)
+   console.log(arrayDePelisFavoritas);
+   // reemplazo el array que tenia la peli como favorita, por el array que ya no la tiene
+   window.sessionStorage.setItem("favorita",JSON.stringify(arrayDePelisFavoritas))
+ }
+
+ console.log(id);
+ console.log(JSON.parse(window.sessionStorage.getItem("favorita")));
 }
